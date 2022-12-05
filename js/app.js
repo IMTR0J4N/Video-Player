5483d2840b32d383940116e0467276abb58942c0
IMTR0J4NV2 <jjorgebeau@edenschool.fr>
2022-12-05 10:35:58 +0100

    Start code button functionnality

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 import '../css/style.css';
 
+const backBtn = document.getElementById('back-btn');
+const skipBtn = document.getElementById('skip-btn');
+const resetBtn = document.getElementById('reset-btn');
+
+const progressBar = document.getElementById('progress-bar');
+const video = document.getElementById('video');
+
+video.loop = true;
+
+video.addEventListener("loadedmetadata", () => {
+    progressBar.max = video.duration
+})
+
+video.addEventListener('click', () => {
+    video.paused ? video.play() : video.pause();
+})
